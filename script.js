1cdc42b34772851500eeb4efa58d01f74f011b27
jeanenriquez <jeanenriqezz@gmail.com>
2023-02-13 17:05:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,5 +17,5 @@ var coordenadas = () => {
 }
 
 btnyes.addEventListener('click', () => {
-    alert('Sabía que dirías que sí🥰 ¡Te amo tanto!');
+    alert('Sabía que dirías que sí💛💐 ¡Te amo tanto! Me alegra mucho decirte que ambos acabamos de ganar: Tú al mejor compañero de vida y yo a la mujer de mis sueños🥰. Voy a hacerte infinitamente feliz lo que me sobra de vida.');
 });
